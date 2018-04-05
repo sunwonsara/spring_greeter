8a94ad013c5e1b4af5c7bb2fa2264bc198cecf02
Jeongseon Won <sunwonsara@naver.com>
2018-04-05 10:06:06 +0900

    정선이에게 인사하는 루틴 작성

--- src/main/java/kr/hs/emirim/sunwonsara/greeter/main/Main.java
+++ src/main/java/kr/hs/emirim/sunwonsara/greeter/main/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 			Greeter greeter = new Greeter();
-			greeter.setFormat("%s, 안녕하세요!");
-			String msg = greeter.greet("스프링"); //msg는 "스프링, 안녕하세요!"가 된다.
+			greeter.setFormat("%s씨, 안녕하세요! 그만와요!");
+			String msg = greeter.greet("정선"); //msg는 "스프링, 안녕하세요!"가 된다.
 			System.out.println(msg);
 
 	}
